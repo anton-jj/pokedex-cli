842840bdfee8c1deaf31f5e1b47e5d9c5d07accd
anton <jappinen.anton@protonmail.com>
2025-08-07 18:38:10 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anton-jj/pokedex-cli
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import ("fmt")
+func main () {
+	fmt.Println("Hello, World!")	
+}
+
+func cleanInput(text string) []string {
+	var input []string
+	return input
+}
