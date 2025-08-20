6d9c9310dc1d9f435bc07bbafdcf8d157c2baaad
anton <jappinen.anton@protonmail.com>
2025-08-20 08:50:52 +0200

    working on map command

--- main.go
+++ main.go
@@ -1,11 +1,108 @@
 package main
 
-import ("fmt")
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+type cliCommand struct {
+	Name string
+	Description string
+	Callback func() error
+}
+
+type Config struct {
+	Next *string
+	Previous *string
+}
+
+type LocationArea struct {
+    Name string `json:"name"`
+    URL  string `json:"url"`
+}
+
+type LocationAreaResponse struct {
+		Count int `json:"count"`
+		Next *string `json:"next"`
+		Previous *string `json:"previous"`
+		Results  []LocationArea `json:"results"`
+}
+	
 func main () {
-	fmt.Println("Hello, World!")	
+
+	scanner := bufio.NewScanner(os.Stdin)
+	cliMap := map[string]cliCommand {
+		"exit" : {
+			Name: "Exit",
+			Description: "Exit the Pokedex",
+			Callback: commandExit,
+		},
+		"map" : {
+			Name : "Map", 
+			Description: "Displays next 20 locations",
+			Calllback: commandMap,
+		},
+	}
+	cliMap["help"] = cliCommand{
+			Name: "Help",
+			Description: "Displays a help message",
+			Callback: func() error {
+				return commandHelp(cliMap)
+			},
+		}
+
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if cmd, ok := cliMap[input[0]]; ok {
+			cmd.Callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+	}
 }
 
 func cleanInput(text string) []string {
-	var input []string
-	return input
+	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+
+func getData(url string) (*ApiResponse, errro ) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var res LocationAreaResponse
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+func getLocations() error {
+	url := "https://pokeapi.co/api/v2/location-area/?offset=20&limit=20"
+	
+}
+
+
+func commandHelp(commands map[string]cliCommand) error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage: \n\n" )
+	for name, cmd := range commands {
+		fmt.Printf("%s: %s\n", name, cmd.Description)
+	}	
+	return nil
 }
--- repl_test.go
+++ repl_test.go
@@ -1,35 +1,41 @@
 package main
 
-import ("testing" 
-	"pokedex-cli"
-)
+import "testing" 
 
 
-func TestCleanInput(t *Testing.T) {
+func TestCleanInput(t *testing.T) {
 
 	cases := []struct {
-	input    string
-	expected []string
-}{
-	{
-		input:    "  hello  world  ",
-		expected: []string{"hello", "world"},
-	},
-	// add more cases here
-}
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input: " h ello     world",
+			expected: []string{"h", "ello", "world"},
+		}, 
+
+		{
+			input: "this is a test",
+			expected: []string{"this", "is", "a", "test"},
+		}, 
+	}
 
 	for _, c := range cases {
-	actual := cleanInput(c.input)
-	// Check the length of the actual slice against the expected slice
-	// if they don't match, use t.Errorf to print an error message
-	// and fail the test
-	for i := range actual {
-		word := actual[i]
-		expectedWord := c.expected[i]
-		// Check each word in the slice
-		// if they don't match, use t.Errorf to print an error message
-		// and fail the test
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) returned %d words, expected %d", c.input, len(actual), len(c.expected))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word  != expectedWord {
+                t.Errorf("cleanInput(%q)[%d] = %q; expected %q", c.input, i, word, expectedWord)
+			}
+		}
 	}
-}
 
 }
